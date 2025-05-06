74a90314f5dcea06542c80addca7d7afd25c8c2f
Daggubati <rajani.eng9@gmail.com>
2025-05-06 19:46:22 -0400

    Add 'Name' folder with app.py, requirements.txt, and Dockerfile

---
+++ Name/app.py
@@ -0,0 +1,14 @@
+
+# app.py
+def main():
+    name = "Sure"
+    email = "sure@gmail.com"
+    phone = "508 569 7389"
+
+    print("Personal Information:")
+    print(f"Name: {name}")
+    print(f"Email: {email}")
+    print(f"Phone: {phone}")
+
+if __name__ == "__main__":
+    main()
